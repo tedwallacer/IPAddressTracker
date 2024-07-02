edbed7670ffc7c45d4396b7f7aab774c1d017901
tedwallacer <163332348+tedwallacer@users.noreply.github.com>
2024-07-02 14:38:18 +0200

    Produced Header.js

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/track-ip">Track IP</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
