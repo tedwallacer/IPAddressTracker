f20d1f3545fd818b0d3650712debd17075ce3bca
tedwallacer <163332348+tedwallacer@users.noreply.github.com>
2024-07-05 12:25:17 +0200

    Develop Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{textAlign: 'center', marginTop: '20px'}}>
+      <p>©2023 IP Address Tracker. All rights reserved.</p>
+      <a href="https://github.com/yourusername/ip-address-tracker" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+      <br/>
